dbfce020964c46ceeaaeea17be57b4ad8b69d962
tmzh <2162408+tmzh@users.noreply.github.com>
2024-01-20 16:53:13 +0800

    use a larger model

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def get_embeds(embeds_dir, prompts):
 
 def generate_and_save_embeds(embeds_dir, prompt):
     text_encoder = T5EncoderModel.from_pretrained(
-        "DeepFloyd/IF-I-M-v1.0",
+        "DeepFloyd/IF-I-L-v1.0",
         subfolder="text_encoder",
         device_map="auto",
         variant="fp16",
@@ -51,7 +51,7 @@ def generate_and_save_embeds(embeds_dir, prompt):
     )
 
     pipe = DiffusionPipeline.from_pretrained(
-        "DeepFloyd/IF-I-M-v1.0",
+        "DeepFloyd/IF-I-L-v1.0",
         text_encoder=text_encoder,  # pass the previously instantiated 8bit text encoder
         unet=None
     )
@@ -66,7 +66,7 @@ def generate_and_save_embeds(embeds_dir, prompt):
 def get_stages():
     # Stage 1 pipeline
     stage_1 = DiffusionPipeline.from_pretrained(
-        "DeepFloyd/IF-I-M-v1.0",
+        "DeepFloyd/IF-I-L-v1.0",
         text_encoder=None,
         variant="fp16",
         torch_dtype=torch.float16,
@@ -75,7 +75,7 @@ def get_stages():
 
     # Stage 2 pipeline
     stage_2 = DiffusionPipeline.from_pretrained(
-        "DeepFloyd/IF-II-M-v1.0",
+        "DeepFloyd/IF-II-L-v1.0",
         text_encoder=None,
         variant="fp16",
         torch_dtype=torch.float16,
@@ -94,11 +94,11 @@ if __name__ == "__main__":
     parser.add_argument("--views", required=True, type=str, 
                         help='Name of views to use. See `get_views` in `views.py`.')
     parser.add_argument("--style", default='', type=str, help='Optional string to prepend prompt with')
-    parser.add_argument("--num_inference_steps", type=int, default=40)
+    parser.add_argument("--num_inference_steps", type=int, default=30)
     parser.add_argument("--num_samples", type=int, default=10)
     parser.add_argument("--reduction", type=str, default='mean')
     parser.add_argument("--seed", type=int, default=0)
-    parser.add_argument("--guidance_scale", type=float, default=7.0)
+    parser.add_argument("--guidance_scale", type=float, default=10.0)
     parser.add_argument("--noise_level", type=int, default=50, help='Noise level for stage 2')
     parser.add_argument("--device", type=str, default='cuda')
     parser.add_argument("--save_metadata", action='store_true',
@@ -129,35 +129,43 @@ if __name__ == "__main__":
 
     stage_1, stage_2 = get_stages()
 
-    # Sample illusions
-    for i in range(args.num_samples):
-        # Admin stuff
-        # generator = torch.manual_seed(args.seed + i)
-        generator = torch.Generator()
-        seed = generator.seed()
-        sample_dir = save_dir / f'{seed:016}'
-        sample_dir.mkdir(exist_ok=True, parents=True)
 
+    def generate_illusion(generator, sample_dir):
         # Sample 64x64 image
         image = sample_stage_1(stage_1,
-                               prompt_embeds,
-                               negative_prompt_embeds,
-                               views,
-                               num_inference_steps=args.num_inference_steps,
-                               guidance_scale=args.guidance_scale,
-                               reduction=args.reduction,
-                               generator=generator)
+                            prompt_embeds,
+                            negative_prompt_embeds,
+                            views,
+                            num_inference_steps=args.num_inference_steps,
+                            guidance_scale=args.guidance_scale,
+                            reduction=args.reduction,
+                            generator=generator)
         save_illusion(image, views, sample_dir)
 
         # Sample 256x256 image, by upsampling 64x64 image
         image = sample_stage_2(stage_2,
-                               image,
-                               prompt_embeds,
-                               negative_prompt_embeds,
-                               views,
-                               num_inference_steps=args.num_inference_steps,
-                               guidance_scale=args.guidance_scale,
-                               reduction=args.reduction,
-                               noise_level=args.noise_level,
-                               generator=generator)
+                            image,
+                            prompt_embeds,
+                            negative_prompt_embeds,
+                            views,
+                            num_inference_steps=args.num_inference_steps,
+                            guidance_scale=args.guidance_scale,
+                            reduction=args.reduction,
+                            noise_level=args.noise_level,
+                            generator=generator)
         save_illusion(image, views, sample_dir)
+
+    # Sample illusions
+    for i in range(args.num_samples):
+        # Set seeds
+        # generator = torch.manual_seed(args.seed + i)
+        generator = torch.Generator()
+        seed = generator.seed()
+
+        # Admin stuff
+        sample_dir = save_dir / f'{seed:016}'
+        sample_dir.mkdir(exist_ok=True, parents=True)
+
+        generate_illusion(generator=generator, sample_dir=sample_dir)
+
+
